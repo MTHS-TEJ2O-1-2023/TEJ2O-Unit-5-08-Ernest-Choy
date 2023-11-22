e76808978b9dbea2887e7ca2b3aa7cbdf2976231
Ernest Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-22 12:20:09 -0500

    tyver not doen

--- main.ts
+++ main.ts
@@ -5,4 +5,20 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+  if (input.buttonIsPressed(Button.A) == true) {
+  distanceToTarget = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+  // move forwards
+  basic.showIcon(IconNames.Yes)
+    if (distanceToTarget >= 10) {
+      robotbit.StpCarTurn(10, 48)
+    } 
+    if (distanceToTarget < 10) {
+      robotbit.StpCarTurn(-10, 48)
+      robotbit.StpCarTurn(90, 10, 125)
+    }
+})
