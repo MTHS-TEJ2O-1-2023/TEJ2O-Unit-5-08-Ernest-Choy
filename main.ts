ccc08aa38af75f39230974778e0f2024d5710ef6
Ernest Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-27 13:13:32 -0500

    tyver

--- main.ts
+++ main.ts
@@ -2,24 +2,25 @@
  *
  * Created by: Ernest
  * Created on: Nov 2023
- * This program moves stepper motors
+ * This program moves stepper motors depending on the distance
 */
 
 // variables
 let distanceToTarget: number = 0
 
-//setup
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 while (true) {
-  if (input.buttonIsPressed(Button.A) == true) {
+  if (input.buttonIsPressed(Button.A) === true) {
     distanceToTarget = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
     // move forwards
     if (distanceToTarget > 10) {
       basic.showString(distanceToTarget.toString())
       robotbit.StpCarMove(10, 48)
     }
+    // move backwards and turn
     if (distanceToTarget <= 10) {
       basic.showString(distanceToTarget.toString())
       robotbit.StpCarMove(-10, 48)
